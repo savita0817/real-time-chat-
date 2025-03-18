345006fe893b4364121dd1e6dda45c311f23cd8c
savita0817 <guptasavita2004@gmail.com>
2025-03-19 00:19:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// WebSocket connection
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+// Start the server
+const PORT = 9000;
+server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
